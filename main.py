1fa98fb2260f2e08c6d028c14578bc1f5881172d
Avi Baruch <avineedswifi@gmail.com>
2026-05-01 04:43:00 -0400

    more brightness changes

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ class AmbientBrightness(LEDModule):
             return 0.9
         if lux > 8:
             return 0.5
-        return 0.05
+        return 0.01
 
     def get_update(self, now):
         self.last_updated = time.time()
